a2d05d5a489d872c10b6aa9a5bcf3ef0b63948a5
fstraining <rdiguangco@futureshop.com>
2013-04-24 08:31:06 -0700

    Create banner_scripts.js
    banner scripts.js

---
+++ banner_scripts.js
@@ -0,0 +1,67 @@
+$(function(){
+ slider.init();
+});
+
+var slider={
+ num:-1,
+ cur:0,
+ cr:[],
+ al:null,
+ at:10*300,
+ ar:true,
+ init:function(){
+  if(!slider.data || !slider.data.length)
+   return false;
+
+  var d=slider.data;
+  slider.num=d.length;
+  var pos=Math.floor(Math.random()*1);//slider.num);
+  for(var i=0;i<slider.num;i++){
+   $('#'+d[i].id).css({left:((i-pos)*780)});
+   $('#slide-nav').append('<a id="slide-link-'+i+'" href="#" onclick="slider.slide('+i+');return false;" onfocus="this.blur();">'+(i+1)+'</a>');
+  }
+
+  $('img,div#slide-controls',$('div#slide-holder')).fadeIn();
+  slider.text(d[pos]); // 
+
+  slider.on(pos); // makes the button active
+  slider.cur=pos;
+  window.setTimeout('slider.auto();',slider.at);
+ },
+ auto:function(){
+  if(!slider.ar)
+   return false;
+
+  var next=slider.cur+1;
+  if(next>=slider.num) next=0;
+  slider.slide(next);
+ },
+ slide:function(pos){
+  if(pos<0 || pos>=slider.num || pos==slider.cur)
+   return;
+
+  window.clearTimeout(slider.al);
+  slider.al=window.setTimeout('slider.auto();',slider.at);
+
+  var d=slider.data;
+  for(var i=0;i<slider.num;i++)
+   $('#'+d[i].id).stop().animate({left:((i-pos)*780)},780,'swing');
+  
+  slider.on(pos);
+  slider.text(d[pos]);
+
+  slider.cur=pos;
+ },
+ on:function(pos){
+  $('#slide-nav a').removeClass('on');
+  $('#slide-nav a#slide-link-'+pos).addClass('on');
+ },
+ text:function(di){
+  $('#slide-client').html(di.client);
+  $('#slide-desc').html(di.desc);
+ },
+ alttext:function(id){
+  $('#slide-client').html(id);
+  $('#slide-desc').html(id);
+ }
+};
